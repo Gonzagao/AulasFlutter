cabc0f5ef0cc1bd7a0335e5eaec47888b15111da
Maicon Gonzaga <sacmaiconchip@gmail.com>
2021-04-19 10:22:45 -0400

    Atividades Obrigatórias
    Entrega das atividades obrigatórias

---
+++ flutter_curso/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_curso/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/03/01/22/18/avatar-2109804_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/05/19/22/03/man-3414477_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@zymail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/20/37/face-1295907_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guigui@abcmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png'),
+};
